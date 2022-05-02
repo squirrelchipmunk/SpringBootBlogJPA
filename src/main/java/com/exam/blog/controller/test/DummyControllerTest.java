fac5eb31cc86708c9b15b79d9cbe48c7120f0bbf
이원준 <ggoong8@gmail.com>
2022-05-02 19:19:36 +0900

    롬복 오류 해결, enum으로 도메인 설정

--- src/main/java/com/exam/blog/controller/test/DummyControllerTest.java
+++ src/main/java/com/exam/blog/controller/test/DummyControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exam.blog.model.RoleType;
 import com.exam.blog.model.User;
 import com.exam.blog.repository.UserRepository;
 
@@ -23,6 +24,7 @@ public class DummyControllerTest {
 		System.out.println("role: "+user.getRole());
 		System.out.println("joinDate: "+user.getJoinDate());
 		
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
 	}
---
+++ src/main/java/com/exam/blog/model/RoleType.java
@@ -0,0 +1,6 @@
+package com.exam.blog.model;
+
+public enum RoleType {
+
+	USER, ADMIN
+}
