961ab96f4ff176f5636ffa58b96b0d430729f19d
이원준 <ggoong8@gmail.com>
2022-05-03 22:47:57 +0900

    update, @Transactional 더티체킹

--- src/main/java/com/exam/blog/controller/test/DummyControllerTest.java
+++ src/main/java/com/exam/blog/controller/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.exam.blog.controller.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exam.blog.model.RoleType;
@@ -81,5 +85,30 @@ public class DummyControllerTest {
 		
 		// Page의 isLast() isFirst() 등으로 페이징 처리 가능
 		return users;
+	}
+	
+	@Transactional // 트랜잭션이 실행되고 메서드가 종료될 때 자동 commit
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id : "+ id);
+		System.out.println("password : "+ requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow( ()-> { // User 영속화
+			return new IllegalArgumentException("수정 실패 : 존재하지 않는 아이디");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		// save() id를 전달하지 않으면 insert
+		//      ''    를 전달하면 update
+		//      '' 를 전달했는데 해당 id가 없으면 insert
+		
+		// save()는 id가 이미 존재하면 업데이트해주는 기능이 있으나 부분 변경 시 없는 데이터를 null로 만듦 >> 기존 데이터를 갖는 인스턴스에 변경 부분을 반영
+		//userRepository.save(user);
+		
+		//더티 체킹 : 영속화된 객체와 비교해서 변경을 감지하고 update 수행
+		return null;
 	}
 }
