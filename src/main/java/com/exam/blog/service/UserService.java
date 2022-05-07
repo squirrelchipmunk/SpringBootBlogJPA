ca222675426186dd668c1392dfd09cd06ec6d681
이원준 <ggoong8@gmail.com>
2022-05-07 22:33:24 +0900

    데이터베이스 격리수준

--- src/main/java/com/exam/blog/service/UserService.java
+++ src/main/java/com/exam/blog/service/UserService.java
@@ -15,8 +15,14 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@Transactional // 전체가 트랜잭션으로 묶여 원자성 유지됨.
+	// 전체가 트랜잭션으로 묶여 원자성 유지됨. 프록시 객체가 생성되어 자동으로 commit 또는 rollback. isolation 옵션으로 격리 수준 설정 가능.
+	@Transactional 
 	public void 회원가입(User user) {
 		userRepository.save(user);
 	}
 }
+
+
+// 오라클 DB 격리 수준 : Read Commit : commit된 부분을 read  >> Phantom Read(데이터가 보였다 안 보였다)/부정합 >> 
+// MySql DB 격리 수준 : Repeatable Read : 부정합 발생하지 않음 >> undo에 변경 로그가 남고 자기 트랜잭션 번호보다 낮은 undo 로그를 select함
+//								     └ 트랜잭션이 시작되기 전 커밋된 내용에 대해 select 
